270ec560be058318093b2ca7d30135d8cbfb7e1e
Ticiana Capris <tcapris@hotmail.com>
2021-11-30 22:55:06 +0000

    add CSS

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Keyboard } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  Container, Title, Form, Input, Submit, List,
+} from './styles';
 
-// import { Container } from './styles';
 
-const Main = () => {
-  return <View />;
+export default function Main() {
+  return (
+    <Container>
+      <Title>Repositórios</Title>
+
+<Form>
+  <Input
+
+    autoCapitalize="none"
+    autoCorrect={false}
+    placeholder="Procurar repositório..."
+  />
+  <Submit onPress={()=>{}}>
+    <Icon name="add" size={22} color="#FFF" />
+  </Submit>
+</Form>
+
+      </Container>
+  );
 }
-
-export default Main;
